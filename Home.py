11bcd8ec3d4a08a974465c1d5e2ef90a4ddfeb47
alexa-per <a01785562@tec.mx>
2026-02-17 17:53:54 -0600

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,17 +1,13 @@
 import streamlit as st
+from utils.state import sistema_sidebar
 
 st.set_page_config(page_title="Despacho Económico", layout="wide")
+
+sistema = sistema_sidebar()
+
 st.title("Simulador de despacho económico (México)")
+st.info(f"Sistema seleccionado: {sistema}")
 
-# Inicializa SOLO una vez
-if "sistema" not in st.session_state:
-    st.session_state.sistema = "SIN"
-
-st.selectbox(
-    "Selecciona el sistema eléctrico:",
-    ["SIN", "BCA", "BCS"],
-    index=["SIN", "BCA", "BCS"].index(st.session_state.sistema),
-    key="sistema",
-)
-
-st.write("DEBUG → sistema en session_state:", st.session_state.sistema)
+st.write("""
+Usa el menú de la izquierda para navegar por módulos.
+""")
