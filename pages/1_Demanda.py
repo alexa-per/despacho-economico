c7a3cec19bf0c52766b0156bb0561fc16d56b02d
alexa-per <a01785562@tec.mx>
2026-02-17 17:38:01 -0600

    Update 1_Demanda.py

--- pages/1_Demanda.py
+++ pages/1_Demanda.py
@@ -3,7 +3,6 @@ import streamlit as st
 st.title("Demanda")
 
 sistema = st.session_state.get("sistema", "SIN")
-
 st.write(f"Sistema activo: {sistema}")
 
 st.write("Aquí irá la descarga de demanda horaria de CENACE.")
