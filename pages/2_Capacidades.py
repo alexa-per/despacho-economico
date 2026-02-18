24ccf84ac849f1dbe3e945d6f8e4d5f6f898d508
alexa-per <a01785562@tec.mx>
2026-02-17 19:03:39 -0600

    Update 2_Capacidades.py

--- pages/2_Capacidades.py
+++ pages/2_Capacidades.py
@@ -1,3 +1,6 @@
 import streamlit as st
+from utils.state import sistema_sidebar
+
+sistema = sistema_sidebar()
 st.title("Capacidades")
 st.write("Aquí definiremos capacidades por tecnología y casos 2024/2026.")
