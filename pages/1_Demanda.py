0a8e1aeedafe7488d63456fbe8f8aa921ec6b1e6
alexa-per <a01785562@tec.mx>
2026-02-18 20:23:50 -0600

    Update 1_Demanda.py

--- pages/1_Demanda.py
+++ pages/1_Demanda.py
@@ -42,7 +42,8 @@ if st.button("Cargar demanda"):
 
     st.subheader("Gráfico")
     st.line_chart(df.set_index("timestamp")["demanda_mw"])
-    csv = df.to_csv(index=False).encode("utf-8")
+
+csv = df.to_csv(index=False).encode("utf-8")
 
 st.download_button(
     "⬇️ Descargar CSV",
